ef55897f51b27a9ea85b26868e9da915d0c0d3bb
ChandlerMcCook <chandler.mccook@gmail.com>
2024-03-21 15:48:25 -0500

    Added some more queries

--- DietQueries.sql
+++ DietQueries.sql
@@ -6,9 +6,22 @@ SELECT * FROM INGREDIENT;
 
 SELECT * FROM PATIENT_INGREDIENT;
 
-SELECT Fname, Ing_id FROM PATIENT p, INGREDIENT i, PATIENT_INGREDIENT
-WHERE (p.P_id = PATIENT_INGREDIENT.PI_Pid 
-AND i.Ing_id = PATIENT_INGREDIENT.PI_Ingid);
+# less efficient way to query using where
+SELECT Fname, Ing_id FROM PATIENT p, INGREDIENT i, PATIENT_INGREDIENT pi
+WHERE (p.P_id = pi.PI_Pid 
+AND i.Ing_id = pi.PI_Ingid);
+
+# more efficient way to query using join
+SELECT Fname, Ing_id FROM PATIENT p 
+INNER JOIN PATIENT_INGREDIENT pi ON p.P_id = pi.PI_Pid 
+INNER JOIN INGREDIENT i ON i.Ing_id = pi.PI_Ingid
+ORDER BY Fname;
+
+# print a list of ingredients and people that are allergic to them
+SELECT Ing_id Ingredient, COALESCE(Fname, 'No Allergies') Patients_Allergic FROM INGREDIENT i 
+LEFT JOIN PATIENT_INGREDIENT pi ON i.Ing_id = pi.PI_Ingid
+LEFT JOIN PATIENT p ON p.P_id = pi.PI_Pid;
+
 
 SELECT FI_Fid Food_Name, FI_Ingid Ingredient FROM FOOD_INGREDIENT
 ORDER BY Food_Name, Ingredient;
