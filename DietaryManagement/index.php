28a9ed1756169f628240d3aaf7160bb915e23372
gracemet <111946144+gracemet@users.noreply.github.com>
2024-03-28 22:41:21 -0500

    Added patient allergy table
    Added patient allergy table with concatenated list
    Added fork knife image
    made fork knife image the logo :)

--- DietaryManagement/index.php
+++ DietaryManagement/index.php
@@ -1,6 +1,6 @@
 <!-- 
 Final Project | Due: 
-By: Matthew Wooten, Chandler McCook, Grace Metri, Alex Larabie, Scott Hartsell
+By: Matthew Wooten, Chandler McCook, Grace Metri
 Description: Dietary Management Database App used by Doctors, Nurses, and 
              Patients to find who can eat what foods under certain
              dietary restrictions and the hospital's inventory.
@@ -19,7 +19,7 @@ Description: Dietary Management Database App used by Doctors, Nurses, and
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <!-- <script src="slideshow.js"></script> -->
   <script src="script.js"></script>
-  <!-- <link id="tab-icon" rel="shortcut icon" type="image/x-icon" href="Images/logo.png" /> -->
+  <link id="tab-icon" rel="shortcut icon" type="image/x-icon" href="Images/SpoonFork.png" />
   <!-- <link rel="preload" id="navBarPic" type="image" href="Images/logoAndName.png" /> -->
  
 
@@ -28,11 +28,11 @@ Description: Dietary Management Database App used by Doctors, Nurses, and
 <body>
   <!-- Nav bar -->
   <ul id="nav">
-    <!-- <li><a style="padding:2px 2px;" href="index.html" target="_self" fetchpriority="high"><img
-          src="Images/logoAndName.png" style="height:60px;display:block;"></a></li> -->
-    <li><a href="login.php" target="_self">Login</a></li>
-    <li><a href="index.php" target="_self">Index</a></li>
+    <li><a style="padding:2px 2px;" href="index.php" target="_self" fetchpriority="high"><img
+          src="Images/SpoonFork.png" style="height:60px;display:block;filter:brightness(0) invert(1);"></a></li>
     <li><a href="about.html" target="_self">About Us</a></li>
+    <li><a href="login.php" target="_self" style="float:right;">Login</a></li>
+    <!-- <li><a href="index.php" target="_self">Index</a></li> -->
   </ul>
 
   <br> <!-- Nav bar end -->
@@ -50,30 +50,64 @@ Description: Dietary Management Database App used by Doctors, Nurses, and
     die("Connection failed: " . $conn->connect_error);
   }
 
-  $sql = "SELECT * FROM PATIENT;";
+  $sql = "SELECT Fname, Ing_id FROM PATIENT p, INGREDIENT i, PATIENT_INGREDIENT
+  WHERE (p.P_id = PATIENT_INGREDIENT.PI_Pid 
+  AND i.Ing_id = PATIENT_INGREDIENT.PI_Ingid);";
   $result = mysqli_query($conn, $sql);
   $size = mysqli_num_rows($result);
 
   echo "<table>";
-        $sql = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='PATIENT'";
-        $columns = mysqli_query($conn, $sql);
+  $sql = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='PATIENT_INGREDIENT'";
+  $columns = array("Patients", "Allergy");
+  echo "<tr>";
+  while ($row = $columns) {
+      foreach ($row as $value)
+          echo "<th>" . $value . "</th>";
+      $columns = null;
+  }
 
-        echo "<tr>";
-        while ($row = mysqli_fetch_assoc($columns)) {
-            foreach ($row as $value)
-                echo "<th>" . $value . "</th>";
-        }
-        echo "</tr>";
+  $table = [];
+  while($row = mysqli_fetch_assoc($result))
+  {
+    if(isset($table[$row["Fname"]])) {
+      $table[$row["Fname"]] = $table[$row["Fname"]] ."," . $row["Ing_id"];
+    }
+    else {
+      $table += [$row["Fname"] => $row["Ing_id"]];
+    }
+  }
 
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "<tr>";
-            foreach ($row as $value) {
-                echo "<td>" . $value . "</td>";
-            }
-            echo "</tr>";
-        }
-        echo "</table>";
-        echo "<br>";
+  foreach($table as $i => $x) {
+    echo "<tr>";
+    echo "<td>" . $i . "</td>";
+    echo "<td>" . $x . "</td>";;
+    echo "</tr>";
+  }
+  echo "</table>";
+  echo "<br>";
+
+
+
+
+//   echo "<table>";
+//   $sql = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='PATIENT_INGREDIENT'";
+//   $columns = mysqli_query($conn, $sql);
+//   echo "<tr>";
+//   while ($row = mysqli_fetch_assoc($columns)) {
+//       foreach ($row as $value)
+//           echo "<th>" . $value . "</th>";
+//   }
+//   echo "</tr>";
+
+//   while ($row = mysqli_fetch_assoc($result)) {
+//       echo "<tr>";
+//       foreach ($row as $value) {
+//           echo "<td>" . $value . "</td>";
+//       }
+//       echo "</tr>";
+//   }
+// echo "</table>";
+// echo "<br>";
 
   ?>
 
